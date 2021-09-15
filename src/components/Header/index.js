9c999a9da6f7c30e631cb33ea62e729cb89fceec
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-09-15 18:05:15 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header(){
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
