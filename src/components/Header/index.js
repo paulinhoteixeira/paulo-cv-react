4fa62ea0619bd144a97d7fe5985ce8d1d03920db
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-09-16 21:46:50 -0300

    ajuste nos links do Header - adicionada a rota para About

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,9 +7,9 @@ export default function Header(){
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="#">About</Link></li>
-                    <li><Link to="#">Projects</Link></li>
-                    <li><Link to="#">Contact</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/projects">Projects</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
