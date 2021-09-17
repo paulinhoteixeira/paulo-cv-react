4482bf4627d232cacb79094a0155fab577a97c21
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-09-17 12:42:39 -0300

    ajutes no conteudo da Home e About

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,59 +1,55 @@
-import PageStructure from '../PageStructure';
+import PageStructure from "../PageStructure";
 
 const About = () => (
   <div>
-      <PageStructure>
-    <h2>Paulo Teixeira de Oliveira</h2>
-    <h2>31 anos</h2>
-    <h2>São Paulo - SP</h2>
-
-    <div>
-      <h2>Education</h2>
-      <p>Ciência da Computação</p>
-      <div></div>
-      <p>Administração</p>
-    </div>
-
-    <div>
-      <h2>Experience</h2>
-      <p>Ciência da Computação</p>
-      <div></div>
-      <p>Administração</p>
-    </div>
-
-    <div>
-      <h2>Skills</h2>
+    <PageStructure>
       <div>
-        <h3>Coding</h3>
-        <p>HTML</p>
+        <h2>Education</h2>
+        <p>Ciência da Computação</p>
         <div></div>
-        <p>CSS</p>
-        <div></div>
-        <p>Javascript</p>
-        <div></div>
-        <p>React</p>
+        <p>Administração</p>
       </div>
 
       <div>
-        <h3>Tools and technologies</h3>
-        <p>Visual Studio Code</p>
+        <h2>Experience</h2>
+        <p>Ciência da Computação</p>
         <div></div>
-        <p>GIT</p>
-        <div></div>
-        <p>Git Hub</p>
-        <div></div>
-        <p>Figma</p>
+        <p>Administração</p>
       </div>
 
       <div>
-        <h3>Project Management</h3>
-        <p>Scrum</p>
-        <div></div>
-        <p>Kanban</p>
-        <div></div>
-        <p>PMI</p>
+        <h2>Skills</h2>
+        <div>
+          <h3>Coding</h3>
+          <p>HTML</p>
+          <div></div>
+          <p>CSS</p>
+          <div></div>
+          <p>Javascript</p>
+          <div></div>
+          <p>React</p>
+        </div>
+
+        <div>
+          <h3>Tools and technologies</h3>
+          <p>Visual Studio Code</p>
+          <div></div>
+          <p>GIT</p>
+          <div></div>
+          <p>Git Hub</p>
+          <div></div>
+          <p>Figma</p>
+        </div>
+
+        <div>
+          <h3>Project Management</h3>
+          <p>Scrum</p>
+          <div></div>
+          <p>Kanban</p>
+          <div></div>
+          <p>PMI</p>
+        </div>
       </div>
-    </div>
     </PageStructure>
   </div>
 );
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,9 +4,25 @@ import PageStructure from "../PageStructure";
 
 const Home = () => (
   <>
-    <PageStructure/>
+    <PageStructure>
+      <h2>Paulo Teixeira de Oliveira</h2>
+      <h2>31 anos</h2>
+      <h2>São Paulo - SP</h2>
+
+      <h2>Profile</h2>
+      <p>
+        Graduado em Ciências da Computação e Administração de Empresas com
+        ênfase em finanças e com certificações SFPC (CertiProf), SFC
+        (SCRUMstudy), CAPM (PMI) e ITIL V3. Experiência profissional em
+        implantação e suporte de sistema de gestão e mais de 8 anos de atuação
+        em Gestão de Projetos em empresas de TI. Altamente comprometido e
+        engajado em encontrar soluções consistentes para as tarefas diárias e
+        metas da empresa, colaborando com ideias, planejamento e estratégias.
+        Tenho facilidade em me adaptar e interagir com novas equipes, ambientes
+        e cultura organizacional. Em transição de carreira para desenvolvimento.
+      </p>
+    </PageStructure>
   </>
-  
 );
 
 export default Home;
