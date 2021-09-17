5df18ad2d6c09edeeb3f105b75741ad2504e97ac
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-09-17 20:10:37 -0300

    adicionada função para expandir as informações no componente InfoBox

--- src/components/InfoBox/index.js
+++ src/components/InfoBox/index.js
@@ -1,14 +1,28 @@
-const InfoBox = ( props ) => (
+import { useState } from "react";
+import Plus from '../assets/plus.svg'
+import CloseButton from '../assets/closeButton.svg'
+
+const InfoBox = (props) => {
+  const [ visible, setVisible ] = useState(false);
+
+  function Visible() {
+    visible ? setVisible(false) : setVisible(true)    
+  }  
+
+  return (
     <div>
-        <h2>{ props.title }</h2>
+      <div>
+        <h2>{props.title}</h2>
+        
+        <img onClick={ Visible } src={visible ? CloseButton : Plus}/>
+      </div>
 
-        <div>
-        { props.children }
-
-        </div>
-
+      {
+        visible && 
+        <div>{props.children}</div>
+      }
     </div>
-
-)
+  );
+};
 
 export default InfoBox;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,22 +1,22 @@
 import PageStructure from "../PageStructure";
 import InfoBox from "../InfoBox";
-import MCDenisDSBio from "../assets/mcDenisBio.PNG"
-import OnlyBio from "../assets/onlyBio.PNG"
+import MCDenisDSBio from "../assets/mcDenisBio.PNG";
+import OnlyBio from "../assets/onlyBio.PNG";
 
 const Projects = () => (
-    <PageStructure>
-
-<InfoBox title="Página da Bio do Instagram da banda Only">
-        <a href="http://onlybio.netlify.app"><img src={OnlyBio} alt="Foto da página da banda Only" /></a>       
-
+  <PageStructure>
+    <InfoBox title="Página da Bio do Instagram da banda Only">
+      <a href="http://onlybio.netlify.app">
+        <img src={OnlyBio} alt="Foto da página da banda Only" />
+      </a>
     </InfoBox>
 
     <InfoBox title="Página da Bio do Instagram do MC Denis DS">
-        <a href="http://mcdenisdsbio.netlify.app"><img src={MCDenisDSBio} alt="Foto da página do MC Denis DS" /></a>       
-
+      <a href="http://mcdenisdsbio.netlify.app">
+        <img src={MCDenisDSBio} alt="Foto da página do MC Denis DS" />
+      </a>
     </InfoBox>
-
-    </PageStructure>
-)
+  </PageStructure>
+);
 
 export default Projects;
