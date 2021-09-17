e25dae15ee72768f383f17dbf62db7df2464cb70
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-09-16 21:14:23 -0300

    adicionados os componentes PageStructure e Abstract

---
+++ src/components/Abstract/index.js
@@ -0,0 +1,18 @@
+const Abstract = () => (
+    <>
+      <div>
+        <h1>Paulo Teixeira</h1>
+        <h2>Front-end developer</h2>
+        <p>Estudante do curso Web Front end & React na Let’s Code.</p>
+      </div>
+      <div>
+        <a href="https://github.com/paulinhoteixeira">Projects</a>
+        <a href="https://onedrive.live.com/download?cid=ED0414E82EC975AE&resid=ED0414E82EC975AE%213273&authkey=AKzzycjT5C2rYm0&em=2">
+          Download CV
+        </a>
+      </div>
+    </>
+    
+  );
+  
+  export default Abstract;
---
+++ src/components/PageStructure/index.js
@@ -0,0 +1,2 @@
+import Header from './Header';
+import Avatar from './Avatar';
