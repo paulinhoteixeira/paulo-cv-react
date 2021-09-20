cbd322e9c1c1c9628118c3512f2c06fe7997f919
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-09-19 21:23:57 -0300

    adicionado o arquivo css do PageStructure para dividir o container em 2 colunas

--- src/components/PageStructure/index.js
+++ src/components/PageStructure/index.js
@@ -1,23 +1,23 @@
-import Header from '../Header';
-import Avatar from '../Avatar';
-import Abstract from '../Abstract';
+import Header from "../Header";
+import Avatar from "../Avatar";
+import Abstract from "../Abstract";
+import "./style.css";
 
-const PageStructure = ( props ) => (
-    <div className="page-structure">
-    <Header/>
+const PageStructure = (props) => (
+  <div className="page-structure">
+    <Header />
 
-    <div className="mainBox">
-    <Abstract/>
-    <Avatar/>
+    <main className="container">
+        <div className="fixedBox">
+        <Abstract />
+        <Avatar />
+        </div>
 
-    </div>
-
-    <div className="dynamicBox">   
-    {props.children}
-
-    </div>
-
-    </div>
-)
+        <div className="dynamicBox">
+            {props.children}
+        </div>
+    </main>
+  </div>
+);
 
 export default PageStructure;
