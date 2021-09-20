bc7da3422d429b50aa644b133493916f493cd8c7
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-09-20 14:42:35 -0300

    adicionado o arquivo css do componente Home. Ajustes nos arquivos css dos componentes Abstract e PageStructure

--- src/components/Abstract/index.js
+++ src/components/Abstract/index.js
@@ -3,17 +3,19 @@ import './style.css'
 
 const Abstract = () => (
     <div className="abstract">
-      <div>
+      <div className="content">
         <div className="nameAndAvatar">
           <h1 className="name">Paulo Teixeira</h1>
           <Avatar/>
         </div>
+        <div >
+        <h2 className="abstractDetails">Front-end developer</h2>
+        <p className="abstractDetails">Estudante do curso Web Front end & React na Let’s Code.</p>
 
-        <h2 className="details">Front-end developer</h2>
-        <p className="details">Estudante do curso Web Front end & React na Let’s Code.</p>
+        </div>
       </div>
       <div className="abstractButtons">
-        <a href="https://github.com/paulinhoteixeira">Projects</a>
+        <a href="https://github.com/paulinhoteixeira">GitHub</a>
         <a href="https://onedrive.live.com/download?cid=ED0414E82EC975AE&resid=ED0414E82EC975AE%213273&authkey=AKzzycjT5C2rYm0&em=2">
           Download CV
         </a>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,33 +1,36 @@
 import React from "react";
 
 import PageStructure from "../PageStructure";
-import InfoBox from "../InfoBox";
+import "./style.css";
 
 const Home = () => (
   <>
     <PageStructure>
-      <h2>Paulo Teixeira de Oliveira</h2>
-      <h2>31 anos</h2>
-      <h2>São Paulo - SP</h2>
+      <div className="home">
+        <div className="homeDetails">
+          <h2>Paulo Teixeira de Oliveira</h2>
+          <p>31 anos</p>
+          <p>Brasileiro</p>
+          <p>Solteiro</p>
+          <p>São Paulo - SP</p>
+        </div>
 
-      <InfoBox title="Profile">
-      <p>
-        Graduado em Ciências da Computação e Administração de Empresas com
-        ênfase em finanças e com certificações SFPC (CertiProf), SFC
-        (SCRUMstudy), CAPM (PMI) e ITIL V3. Experiência profissional em
-        implantação e suporte de sistema de gestão e mais de 8 anos de atuação
-        em Gestão de Projetos em empresas de TI. Altamente comprometido e
-        engajado em encontrar soluções consistentes para as tarefas diárias e
-        metas da empresa, colaborando com ideias, planejamento e estratégias.
-        Tenho facilidade em me adaptar e interagir com novas equipes, ambientes
-        e cultura organizacional. Em transição de carreira para desenvolvimento.
-      </p>
-
-
-      </InfoBox>
-
-     
-     
+        <div className="homeProfile">
+          <h2>Profile</h2>
+          <p>
+            Graduado em Ciências da Computação e Administração de Empresas com
+            ênfase em finanças e com certificações SFPC (CertiProf), SFC
+            (SCRUMstudy), CAPM (PMI) e ITIL V3. Experiência profissional em
+            implantação e suporte de sistema de gestão e mais de 8 anos de
+            atuação em Gestão de Projetos em empresas de TI. Altamente
+            comprometido e engajado em encontrar soluções consistentes para as
+            tarefas diárias e metas da empresa, colaborando com ideias,
+            planejamento e estratégias. Tenho facilidade em me adaptar e
+            interagir com novas equipes, ambientes e cultura organizacional. Em
+            transição de carreira para desenvolvimento.
+          </p>
+        </div>
+      </div>
     </PageStructure>
   </>
 );
