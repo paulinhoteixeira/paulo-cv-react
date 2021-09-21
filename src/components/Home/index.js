23891b9f52fce0d4d757027ac165671209c941c1
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-09-21 19:44:05 -0300

    alterando a home e deixando apenas os componentes Header e Abstract

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,37 +1,20 @@
 import React from "react";
 
-import PageStructure from "../PageStructure";
+import Header from "../Header";
+import Abstract from "../Abstract";
+
+import './style.css'
 
 const Home = () => (
   <>
-    <PageStructure>
-      <div className="home">
-        <div className="homeDetails">
-          <h2>Paulo Teixeira de Oliveira</h2>
-          <p>31 anos</p>
-          <p>Brasileiro</p>
-          <p>Solteiro</p>
-          <p>São Paulo - SP</p>
-        </div>
+  <div>
+    <Header />
 
-        <div className="homeProfile">
-          <h2>Profile</h2>
-          <p>
-            Graduado em Ciências da Computação e Administração de Empresas com
-            ênfase em finanças e com certificações SFPC (CertiProf), SFC
-            (SCRUMstudy), CAPM (PMI) e ITIL V3. Experiência profissional em
-            implantação e suporte de sistema de gestão e mais de 8 anos de
-            atuação em Gestão de Projetos em empresas de TI. Altamente
-            comprometido e engajado em encontrar soluções consistentes para as
-            tarefas diárias e metas da empresa, colaborando com ideias,
-            planejamento e estratégias. Tenho facilidade em me adaptar e
-            interagir com novas equipes, ambientes e cultura organizacional. Em
-            transição de carreira para desenvolvimento.
-          </p>
-        </div>
-      </div>
-    </PageStructure>
+  </div>
+    <div className="abstractHome">
+    <Abstract/>
+
+    </div>
   </>
 );
-
 export default Home;
