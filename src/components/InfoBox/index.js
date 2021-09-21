b706d1c7fba1f4e61b79cc8e826612ab97ff1707
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-09-21 13:07:30 -0300

    imagens puls e closeButton alteradas, adicionado o css do componente InfoBox

--- src/components/InfoBox/index.js
+++ src/components/InfoBox/index.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Plus from '../assets/plus.svg'
 import CloseButton from '../assets/closeButton.svg'
 
+import './style.css';
+
 const InfoBox = (props) => {
   const [ visible, setVisible ] = useState(false);
 
@@ -10,8 +12,8 @@ const InfoBox = (props) => {
   }  
 
   return (
-    <div>
-      <div>
+    <div className="infoBox">
+      <div className="infoBoxTitle">
         <h2>{props.title}</h2>
         
         <img onClick={ Visible } src={visible ? CloseButton : Plus} alt="Icone para abrir e fechar conteúdo"/>
