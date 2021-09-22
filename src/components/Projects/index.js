ddcc90a13c902b4ad6f9369b8326c7e31edd2834
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-09-22 20:24:46 -0300

    adicionado o css do componente Projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,19 +3,26 @@ import InfoBox from "../InfoBox";
 import MCDenisDSBio from "../assets/mcDenisBio.PNG";
 import OnlyBio from "../assets/onlyBio.PNG";
 
+import './style.css'
+
 const Projects = () => (
   <PageStructure>
+    <div className="projects">
+    <h1 className="projectsTitle">Projects</h1>
+
     <InfoBox title="Página da Bio do Instagram da banda Only">
-      <a href="http://onlybio.netlify.app">
+      <a className="projectsLink" href="http://onlybio.netlify.app">
         <img src={OnlyBio} alt="Foto da página da banda Only" />
       </a>
     </InfoBox>
 
     <InfoBox title="Página da Bio do Instagram do MC Denis DS">
-      <a href="http://mcdenisdsbio.netlify.app">
+      <a className="projectsLink" href="http://mcdenisdsbio.netlify.app">
         <img src={MCDenisDSBio} alt="Foto da página do MC Denis DS" />
       </a>
     </InfoBox>
+
+    </div>
   </PageStructure>
 );
 
